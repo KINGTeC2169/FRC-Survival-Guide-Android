4d8d0dafa59e75063267822b74823bb9aa807c5d
Ryan Zoeller <rtzoeller@hotmail.com>
2013-07-05 15:58:11 -0500

    Chaned the reminder for tablet users to pick randomly from a list of strings instead of displaying fixed content

--- TestApp/src/main/java/com/rtzoeller/PromptSelectWeekFragment.java
+++ TestApp/src/main/java/com/rtzoeller/PromptSelectWeekFragment.java
@@ -7,19 +7,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by rtzoeller on 7/3/13.
  */
 public class PromptSelectWeekFragment extends Fragment {
+
+    private static final List<String> PROMPT_LIST = new ArrayList<String>();
+
+    static {
+        PROMPT_LIST.add("Pick a week to learn about!");
+        PROMPT_LIST.add("Go ahead, click a week and get started!");
+        PROMPT_LIST.add("Well, what are you waiting for?");
+    }
 
     public PromptSelectWeekFragment() { // Empty public constructor mandated by the fragment manager
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Random random = new Random();
+        String content = PROMPT_LIST.get(random.nextInt(PROMPT_LIST.size()));
 
         View view = inflater.inflate(R.layout.fragment_prompt_select_week, container, false);
-        ((TextView)view.findViewById(R.id.prompt)).setText("Pick a week to learn about!");
+        ((TextView)view.findViewById(R.id.prompt)).setText(content);
         return view;
     }
 }
