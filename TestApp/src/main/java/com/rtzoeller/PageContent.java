f5541eb34cd80117e6dd744984bc2cb319ef5006
Ryan Zoeller <rtzoeller@hotmail.com>
2013-07-25 08:07:03 -0500

    Added a page about Prototyping Fixed a bug causing the PAGE_MAP cache to be refilled several times per page change instead of only being created the first time

--- TestApp/src/main/java/com/rtzoeller/PageContent.java
+++ TestApp/src/main/java/com/rtzoeller/PageContent.java
@@ -12,14 +12,17 @@ public class PageContent {
     // Stores the items for the Pages
     public static Map<Key, Page> PAGE_MAP = new HashMap<Key, Page>();
     // Number of pages in each week
-    public static final int numPages[] = {3,0,0,0,0,0};
+    public static final int numPages[] = {3,1,0,0,0,0};
 
     // Load the pages from the application resources
     PageContent(Context context) {
-        PAGE_MAP.put(new Key(1, 0), new Page(context, R.array.brainstorming));
-        PAGE_MAP.put(new Key(1, 1), new Page(context, R.array.approaching_the_game));
-        PAGE_MAP.put(new Key(1, 2), new Page(context, R.array.game_rules));
+        if (PAGE_MAP.isEmpty()) { // Only fill the map once
+            PAGE_MAP.put(new Key(1, 0), new Page(context, R.array.brainstorming));
+            PAGE_MAP.put(new Key(1, 1), new Page(context, R.array.approaching_the_game));
+            PAGE_MAP.put(new Key(1, 2), new Page(context, R.array.game_rules));
 
+            PAGE_MAP.put(new Key(2, 0), new Page(context, R.array.prototyping));
+        }
     }
 
     // A Key implementation allowing us to do a 2d lookup using a HashMap
