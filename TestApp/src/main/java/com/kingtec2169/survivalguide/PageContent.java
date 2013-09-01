3c4f32d718b0ddc8ce824c08ef4defc0f5a60e7d
Ryan Zoeller <rtzoeller@hotmail.com>
2013-09-01 17:02:28 -0500

    Rewrote the PageContent.get() lookup to use a ArrayList instead of a HashMap, removing the Key() dependency as well Lookups should be faster as they occur in O(1) time and don't require a Key() allocation

--- TestApp/src/main/java/com/kingtec2169/survivalguide/PageContent.java
+++ TestApp/src/main/java/com/kingtec2169/survivalguide/PageContent.java
@@ -2,15 +2,15 @@ package com.kingtec2169.survivalguide;
 
 import android.content.Context;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rtzoeller on 6/27/13.
  */
 public class PageContent {
     // Stores the items for the Pages
-    public static Map<Key, Page> PAGE_MAP = new HashMap<Key, Page>();
+    public static List<Page> PAGES = new ArrayList<Page>();
     // Number of pages in each week
     public static final int numPages[][] = {
             {3,3,0,0,0,0},
@@ -24,7 +24,7 @@ public class PageContent {
 
     // Load the pages from the application resources
     PageContent(Context context) {
-        if (PAGE_MAP.isEmpty()) { // If no content has been loaded set the loaded variable to reflect that
+        if (PAGES.isEmpty()) { // If no content has been loaded set the loaded variable to reflect that
             loaded_group = -1;
             loaded_child = -1;
         }
@@ -37,7 +37,7 @@ public class PageContent {
             unload(loaded_group, loaded_child);
             load(group, child);
         }
-        return PAGE_MAP.get(new Key(group, child, position));
+        return PAGES.get(position);
     }
 
     private static void load (int group, int child) {
@@ -47,14 +47,14 @@ public class PageContent {
             case 0:
                 switch (child) {
                     case 0:
-                        PAGE_MAP.put(new Key(0, 0, 0), new Page(mContext, R.array.brainstorming));
-                        PAGE_MAP.put(new Key(0, 0, 1), new Page(mContext, R.array.approaching_the_game));
-                        PAGE_MAP.put(new Key(0, 0, 2), new Page(mContext, R.array.game_rules));
+                        PAGES.add(new Page(mContext, R.array.brainstorming));
+                        PAGES.add(new Page(mContext, R.array.approaching_the_game));
+                        PAGES.add(new Page(mContext, R.array.game_rules));
                         break;
                     case 1:
-                        PAGE_MAP.put(new Key(0, 1, 0), new Page(mContext, R.array.getting_materials));
-                        PAGE_MAP.put(new Key(0, 1, 1), new Page(mContext, R.array.prototyping));
-                        PAGE_MAP.put(new Key(0, 1, 2), new Page(mContext, R.array.chassis_building));
+                        PAGES.add(new Page(mContext, R.array.getting_materials));
+                        PAGES.add(new Page(mContext, R.array.prototyping));
+                        PAGES.add(new Page(mContext, R.array.chassis_building));
                         break;
                 }
                 break;
@@ -64,57 +64,7 @@ public class PageContent {
     private static void unload (int group, int child) {
         loaded_group = -1;
         loaded_child = -1;
-        switch (group) {
-            case 0:
-                switch (child) {
-                    case 0:
-                        PAGE_MAP.remove(new Key(0, 0, 0));
-                        PAGE_MAP.remove(new Key(0, 0, 1));
-                        PAGE_MAP.remove(new Key(0, 0, 2));
-                        break;
-                    case 1:
-                        PAGE_MAP.remove(new Key(0, 1, 0));
-                        PAGE_MAP.remove(new Key(0, 1, 1));
-                        PAGE_MAP.remove(new Key(0, 1, 2));
-                        break;
-                }
-                break;
-        }
-    }
-
-    // A Key implementation allowing us to do a 3d lookup using a HashMap
-    static class Key {
-
-        private final int x;
-        private final int y;
-        private final int z;
-
-        public Key(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Key key = (Key) o;
-
-            if (x != key.x) return false;
-            if (y != key.y) return false;
-            if (z != key.z) return false;
-
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = x;
-            result = 31 * result + y;
-            result = 31 * result + z;
-            return result;
-        }
+        // Clear array to be refilled with new data
+        PAGES.clear();
     }
 }
