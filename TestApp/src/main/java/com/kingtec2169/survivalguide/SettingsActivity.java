5e76a329cf9faea0f51c85a1f917e6ea3df7ec25
Ryan Zoeller <rtzoeller@hotmail.com>
2013-09-01 16:19:33 -0500

    Moved code from package com.rtzoeller to com.kingtec2169.survivalguide to prepare for release Please note, the app will not overwrite installs from before this commit. Care should be taken when working with commits from before this point.

--- TestApp/src/main/java/com/kingtec2169/survivalguide/SettingsActivity.java
+++ TestApp/src/main/java/com/kingtec2169/survivalguide/SettingsActivity.java
@@ -1,4 +1,4 @@
-package com.rtzoeller;
+package com.kingtec2169.survivalguide;
 
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
--- TestApp/src/main/java/com/kingtec2169/survivalguide/WeekListActivity.java
+++ TestApp/src/main/java/com/kingtec2169/survivalguide/WeekListActivity.java
@@ -1,4 +1,4 @@
-package com.rtzoeller;
+package com.kingtec2169.survivalguide;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
