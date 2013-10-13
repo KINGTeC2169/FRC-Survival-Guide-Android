fe5ae52439180b40c0f4e6f7bb3ecb73bb88c8d6
Ryan Zoeller <rtzoeller@hotmail.com>
2013-10-13 12:18:25 -0500

    Fixed a bug causing the action bar not to show up in the Settings windows on Honeycomb devices Fixed an invalid transformation being applied on pre-Honeycomb devices

--- TestApp/src/main/java/com/kingtec2169/survivalguide/ZoomOutPageTransformer.java
+++ TestApp/src/main/java/com/kingtec2169/survivalguide/ZoomOutPageTransformer.java
@@ -1,5 +1,6 @@
 package com.kingtec2169.survivalguide;
 
+import android.annotation.SuppressLint;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
@@ -10,6 +11,7 @@ public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
     private static float MIN_SCALE = 0.85f;
     private static float MIN_ALPHA = 0.5f;
 
+    @SuppressLint("NewApi")
     public void transformPage(View view, float position) {
         int pageWidth = view.getWidth();
         int pageHeight = view.getHeight();
@@ -21,12 +23,12 @@ public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
         } else if (position <= 1) { // [-1,1]
             // Modify the default slide transition to shrink the page as well
             float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
-            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+            float verticalMargin = pageHeight * (1 - scaleFactor) / 2;
+            float horizontalMargin = pageWidth * (1 - scaleFactor) / 2;
             if (position < 0) {
-                view.setTranslationX(horzMargin - vertMargin / 2);
+                view.setTranslationX(horizontalMargin - verticalMargin / 2);
             } else {
-                view.setTranslationX(-horzMargin + vertMargin / 2);
+                view.setTranslationX(-horizontalMargin + verticalMargin / 2);
             }
 
             // Scale the page down (between MIN_SCALE and 1)
