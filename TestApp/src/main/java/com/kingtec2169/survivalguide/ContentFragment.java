097533fe0bb6feef518a1b93c4faef94beb96ffb
rtzoeller <rtzoeller@localhost>
2013-10-30 14:41:37 -0500

    Added support for tabbed swiping

--- TestApp/src/main/java/com/kingtec2169/survivalguide/ContentFragment.java
+++ TestApp/src/main/java/com/kingtec2169/survivalguide/ContentFragment.java
@@ -3,7 +3,11 @@ package com.kingtec2169.survivalguide;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,12 +25,8 @@ public class ContentFragment extends Fragment {
     public static final String ARG_CHILD_ID = "child_id";
     public static final String ARG_PAGE_ID = "page_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private NavigationListItem mItem;
     private ViewPager mViewPager = null;
-    private int mSetPage;
+    private int mSetPage = 0;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -39,11 +39,55 @@ public class ContentFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments().containsKey(ARG_CHILD_ID)) {
-            mItem = NavigationListContent.CHILDREN.get(getArguments().getInt(ARG_GROUP_ID)).get(getArguments().getInt(ARG_CHILD_ID));
-        }
+        // Is there a specific page we should display
         if (getArguments().containsKey(ARG_PAGE_ID)) {
             mSetPage = getArguments().getInt(ARG_PAGE_ID);
+        }
+
+        // If we have access to an ActionBar,
+        // display tabs representing the pages available
+        ActionBar actionBar = ((ActionBarActivity)getActivity()).getSupportActionBar();
+        if (getArguments().containsKey(ARG_GROUP_ID) && getArguments().containsKey(ARG_CHILD_ID) && actionBar != null) {
+            int group = getArguments().getInt(ARG_GROUP_ID);
+            int child = getArguments().getInt(ARG_CHILD_ID);
+
+            // TODO: Move ActionBar calls to the activity
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.removeAllTabs();
+
+            ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+                @Override
+                public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+                    // When the tab is selected, switch to the
+                    // corresponding page in the ViewPager.
+                    if (mViewPager != null) {
+                        mViewPager.setCurrentItem(tab.getPosition());
+                    }
+                }
+
+                @Override
+                public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+                }
+
+                @Override
+                public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+                }
+            };
+
+            // Add a tab for each page of the set
+            // Using the titles from each page.
+            for (int i = 0; i < PageContent.numPages[group][child]; i++) {
+                Log.e("TitleOf", "" + group + " " + child + " " + i);
+                actionBar.addTab(actionBar.newTab()
+                        .setText(PageFragment.getTitle(getActivity(), group, child, i))
+                        .setTabListener(tabListener));
+            }
+
+            // Select the correct page
+            // This becomes necessary when using the Search function in the app
+            actionBar.setSelectedNavigationItem(mSetPage);
         }
     }
 
@@ -61,6 +105,20 @@ public class ContentFragment extends Fragment {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             pager.setPageTransformer(true, new ZoomOutPageTransformer());
         }
+
+        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // When swiping between tabs, set the correct tab as selected
+                super.onPageSelected(position);
+                if (getActivity() != null) {
+                    ActionBar actionBar = ((ActionBarActivity)getActivity()).getSupportActionBar();
+                    if (actionBar!= null) {
+                        actionBar.setSelectedNavigationItem(position);
+                    }
+                }
+            }
+        });
         mViewPager = pager;
         return(result);
     }
