900c47cf39d230345aa431095decb1c70345552f
rtzoeller <rtzoeller@localhost>
2013-11-12 12:10:51 -0600

    Compiled an alpha apk which was pushed to the Play Store as 0.1.3

--- TestApp/src/main/java/com/kingtec2169/survivalguide/Page.java
+++ TestApp/src/main/java/com/kingtec2169/survivalguide/Page.java
@@ -7,6 +7,9 @@ import android.content.Context;
  */
 public class Page {
     // Holder class for the Pages in the ViewPager
+    // How many items this entry should have
+    public int expectedLength;
+    // Information loaded from XML
     public int layoutResourceId;
     public String text;
     public String title;
@@ -15,18 +18,47 @@ public class Page {
 
     public Page(Context context, String[] contentResources) { // Accepts a calling context and an array of the page data
         super();
-        if(contentResources.length == 5) { // Make sure we have received a valid array
-            this.layoutResourceId = context.getResources().getIdentifier(contentResources[0], "layout", context.getPackageName());
-            this.title = contentResources[1];
-            this.text = contentResources[2];
-            this.imageResourceId = context.getResources().getIdentifier(contentResources[3], "drawable", context.getPackageName());
-            this.tags = contentResources[4];
+
+        if(contentResources.length >= 1) {
+            // Make sure there is an entry for the layout of the expected view
+            // This will tell us how long of an XML array we should expect
+
+            // Get the layout resource id from the name stored in XML
+            layoutResourceId = context.getResources().getIdentifier(contentResources[0], "layout", context.getPackageName());
+            // Look up the expected length for this layout
+            expectedLength = getExpectedLength(layoutResourceId);
+
+            // Make sure the expected length matches what we are actually working with
+            if(contentResources.length == expectedLength) {
+                // Store the XML data
+                switch (layoutResourceId) {
+                    case R.layout.fragment_pager_item:
+                        this.title = contentResources[1];
+                        this.text = contentResources[2];
+                        this.imageResourceId = context.getResources().getIdentifier(contentResources[3], "drawable", context.getPackageName());
+                        this.tags = contentResources[4];
+                        break;
+                }
+            }
+        } else {
+            // Something is wrong with the XML data
+            expectedLength = 0;
         }
     }
 
     public Page(Context context, int arrayResourceId) { // Accepts a calling context and a resource id for the array of the page data
         this(context, context.getResources().getStringArray(arrayResourceId));
+    }
 
+    public static int getExpectedLength(int layoutResourceId) {
+        // These numbers should include the layout tag in their length
+        switch (layoutResourceId) {
+            case R.layout.fragment_pager_item:
+                return 5;
+            default:
+                // Something is wrong
+                return 0;
+        }
     }
 
 }
