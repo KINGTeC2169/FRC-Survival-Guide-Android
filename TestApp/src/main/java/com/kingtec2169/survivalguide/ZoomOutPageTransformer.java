48dc9f41088824f5ba165fc9b808da09f7d56501
Ryan Zoeller <rtzoeller@hotmail.com>
2014-08-09 22:43:08 -0500

    Removed a decoration suppressing lint from warning about an unsupported API call since it is now supported by all devices.

--- TestApp/src/main/java/com/kingtec2169/survivalguide/ZoomOutPageTransformer.java
+++ TestApp/src/main/java/com/kingtec2169/survivalguide/ZoomOutPageTransformer.java
@@ -5,7 +5,6 @@ package com.kingtec2169.survivalguide;
 // scales the content based on its distance from the equilibrium and also makes it
 // translucent.
 
-import android.annotation.SuppressLint;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
@@ -13,7 +12,6 @@ public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
     private static final float MIN_SCALE = 0.85f;
     private static final float MIN_ALPHA = 0.5f;
 
-    @SuppressLint("NewApi")
     public void transformPage(View view, float position) {
         int pageWidth = view.getWidth();
         int pageHeight = view.getHeight();
