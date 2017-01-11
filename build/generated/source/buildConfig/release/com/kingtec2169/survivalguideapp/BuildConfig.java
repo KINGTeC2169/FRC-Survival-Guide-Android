4fb2d8954be55264e16e682b76f20e8b03e8966e
Cole Greenlee <ColeTGreenlee@gmail.com>
2017-01-10 19:45:40 -0600

    The Big v2 Update
    Implemented material design
    New Icons
    Aded Friebase Integrations
    --Notifications
    --Analytics
    --Crash Reporting

---
+++ build/generated/source/buildConfig/release/com/kingtec2169/survivalguideapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kingtec2169.survivalguideapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.kingtec2169.survivalguideapp";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "";
+}
